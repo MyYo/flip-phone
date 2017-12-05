134188b250f7d368919463ad6df1c91ad372426e
Chandler Watson <watson@facni.com>
2017-12-05 15:46:00 -0800

    [Minor] Add Driver_Flash.cpp

---
+++ Code/FlightCode/Driver_Flash.cpp
@@ -0,0 +1,41 @@
+#include "Driver_Flash.h"
+
+#include <SPI.h>
+#include <Adafruit_SPIFlash.h>
+#include <Adafruit_SPIFlash_FatFs.h>
+
+#define FLASH_TYPE     SPIFLASHTYPE_W25Q16BV
+#define FLASH_SS       SS1
+#define FLASH_SPI_PORT SPI1
+#define FILE_NAME      "data.txt"
+
+File dataFile;
+
+void Flash_Init()
+{
+    if (!flash.begin(FLASH_TYPE)) 
+    {
+        Serial.println("Could not initialize flash!");
+        return;
+    }
+    if (!fatfs.begin()) 
+    {
+        Serial.println("Could not mount FS!");
+        return;
+    }
+    dataFile = fatfs.open(FILE_NAME, FILE_WRITE);
+    if (!dataFile) 
+    {
+        Serial.println("Could not open file to log to!");
+    }
+}
+
+void Flash_Log(String message)
+{
+	dataFile.println(message);
+}
+
+void Flash_Close()
+{
+	dataFile.close();
+}
