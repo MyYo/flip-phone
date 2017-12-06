615ff0321a20e95bfdf710664674fccda257d14f
Chandler <watson@facni.com>
2017-12-05 18:00:27 -0800

    [Minor] Add caching for flash logging

--- Code/FlightCode/Driver_Flash.cpp
+++ Code/FlightCode/Driver_Flash.cpp
@@ -4,12 +4,15 @@
 #include <Adafruit_SPIFlash.h>
 #include <Adafruit_SPIFlash_FatFs.h>
 
-#define FLASH_TYPE     SPIFLASHTYPE_W25Q16BV
-#define FLASH_SS       SS1
-#define FLASH_SPI_PORT SPI1
+#define FLASH_TYPE      SPIFLASHTYPE_W25Q16BV
+#define FLASH_SS        SS1
+#define FLASH_SPI_PORT  SPI1
+#define CACHE_SIZE      100 
 #define FILE_NAME      "data.txt"
 
 File flashDataFile;
+String cache[CACHE_SIZE];
+int cache_head = 0;
 Adafruit_SPIFlash flash(FLASH_SS, &FLASH_SPI_PORT);
 Adafruit_W25Q16BV_FatFs fatfs(flash);
 
@@ -34,10 +37,22 @@ void Flash_Init()
 
 void Flash_Log(String message)
 {
-	flashDataFile.println(message);
+    if (cache_head < CACHE_SIZE)
+    {
+        cache[cache_head] = message;
+        cache_head++;
+    }
+    else
+    {
+        Serial.println("Log cache has run out of memory!");
+    }
 }
 
 void Flash_Close()
 {
+    for (int i = 0; i < cache_head; i++)
+    {
+        flashDataFile.println(cache[i]);
+    }
 	flashDataFile.close();
 }
