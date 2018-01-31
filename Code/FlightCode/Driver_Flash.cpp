b567d949838df6d97cbfe7f35f7ca089ec2c07b0
Chandler <watson@facni.com>
2018-01-30 16:17:42 -0800

    Add brief advisory to Driver_Flash.cpp

--- Code/FlightCode/Driver_Flash.cpp
+++ Code/FlightCode/Driver_Flash.cpp
@@ -7,8 +7,12 @@
 #define FLASH_TYPE      SPIFLASHTYPE_W25Q16BV
 #define FLASH_SS        SS1
 #define FLASH_SPI_PORT  SPI1
-#define CACHE_SIZE      100 
-#define FILE_NAME      "data.txt"
+#define CACHE_SIZE      1000
+#define FILE_NAME       "data.txt"
+
+// IMPORTANT: before running this code, run fatfs_format.ino (example in the 
+// the Adafruit_SPIFlash library) to add the FAT file system. If a "log cache
+// has run out of memory" error occurs, increase CACHE_SIZE to match. 
 
 File flashDataFile;
 String cache[CACHE_SIZE];
@@ -20,7 +24,7 @@ void Flash_Init()
 {
     if (!flash.begin(FLASH_TYPE)) 
     {
-        Serial.println("Could not initialize flash!");
+        Serial.println("Could not initialize flash! See Driver_Flash.cpp");
         return;
     }
     if (!fatfs.begin()) 
@@ -31,7 +35,7 @@ void Flash_Init()
     flashDataFile = fatfs.open(FILE_NAME, FILE_WRITE);
     if (!flashDataFile) 
     {
-        Serial.println("Could not open file to log to!");
+        Serial.println("Could not open file to log to! See Driver_Flash.cpp");
     }
 }
 
@@ -44,7 +48,7 @@ void Flash_Log(String message)
     }
     else
     {
-        Serial.println("Log cache has run out of memory!");
+        Serial.println("Log cache has run out of memory! See Driver_Flash.cpp");
     }
 }
 
