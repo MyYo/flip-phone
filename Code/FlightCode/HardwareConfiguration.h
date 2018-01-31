89bb2ef8f3678ece1fb97946a7300b17fe9bbb59
MyYo <MyYo@users.noreply.github.com>
2018-01-30 19:45:51 -0800

    [Minor] Updated motor pins

--- Code/FlightCode/HardwareConfiguration.h
+++ Code/FlightCode/HardwareConfiguration.h
@@ -3,16 +3,15 @@
 //This file defines the hardware configuration of the system
 
 //Pin table (Re-define depending on actual hardware configuration)
-#define PIN_SD_CARD_DETECT		9	//SDCard Port CD
-#define PIN_SD_CHIP_SELECT		8	//SDCard Port CS
-#define PIN_PING_DOWNFACING		A0  //Ping Port SIG
-#define PIN_PING_UPFACING		A1  //Ping Port SIG
-#define PIN_I2C_SCL				A5	//Used by IMU
-#define PIN_I2C_SDA				A4	//Used by IMU
-#define PIN_MOTOR_INA     1  //TBD Motor
-#define PIN_MOTOR_INB     2  //TBD Motor
-#define PIN_MOTOR_PWM     3  //TBD Motor
-
+#define PIN_SD_CARD_DETECT   9	 //SDCard Port CD
+#define PIN_SD_CHIP_SELECT   8	 //SDCard Port CS
+#define PIN_PING_DOWNFACING  A0  //Ping Port SIG
+#define PIN_PING_UPFACING    A1  //Ping Port SIG
+#define PIN_I2C_SCL		     A5	 //Used by IMU
+#define PIN_I2C_SDA		     A4	 //Used by IMU
+#define PIN_MOTOR_INA  11  //Motor
+#define PIN_MOTOR_PWM  10  //Motor
+#define PIN_MOTOR_INB  09  //Motor
 
 //Select whether to log data through the com connection or SD card
 //#define LOG_TO_COM
